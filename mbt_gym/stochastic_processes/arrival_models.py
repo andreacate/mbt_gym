25d534e1e0a67c28a491b1fd5afae62e663b9231
Andrea Catelli <154093000+andreacate@users.noreply.github.com>
2025-09-08 12:31:59 +0200

    Added support for the arrival model for Fads, Informed and Uniformed traders

--- mbt_gym/stochastic_processes/arrival_models.py
+++ mbt_gym/stochastic_processes/arrival_models.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 import numpy as np
 
+from mbt_gym.gym.index_names import ASSET_PRICE_INDEX, FADS_INDEX
 from mbt_gym.stochastic_processes.StochasticProcessModel import StochasticProcessModel
 
 
@@ -127,3 +128,56 @@ class HawkesArrivalModel(ArrivalModel):
 
     # TODO: Improve this with 4*std
     # See: https://math.stackexchange.com/questions/4047342/expectation-of-hawkes-process-with-exponential-kernel
+
+class FadsInformedUniformedTradersArrivalModel(ArrivalModel):
+    def __init__(
+        self,
+        baseline_arrival_rate: np.ndarray = np.array([[0.0, 0.0]]),
+        step_size: float = 0.01,
+        phi: float = 0.5,
+        psi: float = 0.5,
+        k: float = 0.5,
+        gamma: float = 0.5,
+        fads_proportion: float = 0.5,
+        sigma: float = 0.5,
+
+        terminal_time: float = 1,
+        num_trajectories: int = 1,
+        seed: Optional[int] = None,
+    ):
+        self.baseline_arrival_rate = baseline_arrival_rate
+        # self.jump_size = jump_size  # see https://arxiv.org/pdf/1507.02822.pdf, equation (4).
+        # self.mean_reversion_speed = mean_reversion_speed
+        self.phi = phi
+        self.psi = psi
+        self.k = k
+        self.gamma = gamma
+        self.fads_proportion = fads_proportion
+        self.sigma = sigma
+        super().__init__(
+            min_value=np.array([[0, 0]]),
+            max_value=np.array([[1, 1]]) * self._get_max_arrival_rate(),
+            step_size=step_size,
+            terminal_time=terminal_time,
+            initial_state=baseline_arrival_rate,
+            num_trajectories=num_trajectories,
+            seed=seed,
+        )
+
+    def update(self, arrivals: np.ndarray, fills: np.ndarray, actions: np.ndarray, state: np.ndarray = None) -> np.ndarray:
+        
+        # current state is (num_trajectories, 2) meaning for each trajectory we have a buy and sell arrival rate
+        # at every step (so time istante) we update the arrival rate according to the formula, so will depend on spread of previous round and the fad
+        # TODO check what is S- and S+ in the paper for the boundary cases
+        #self.current_state[:,0] = self.phi * np.exp(-self.k * actions[:, 0]) + self.psi * np.exp(-self.gamma * actions[:, 0]) - self.gamma * (self.sigma * self.fads_proportion * state[:, FADS_INDEX])
+        #self.current_state[:,1] = self.phi * np.exp(-self.k * actions[:, 1]) + self.psi * np.exp(-self.gamma * actions[:, 1]) + self.gamma * (self.sigma * self.fads_proportion * state[:, ASSET_PRICE_INDEX])
+        self.current_state[:,0] = self.phi * np.exp(-self.k * actions[:, 0]) + self.psi * np.exp(-self.gamma * actions[:, 0]) - self.gamma * (self.sigma * self.fads_proportion * np.maximum(state[:, FADS_INDEX], state[:, ASSET_PRICE_INDEX]))
+        self.current_state[:,1] = self.phi * np.exp(-self.k * actions[:, 1]) + self.psi * np.exp(-self.gamma * actions[:, 1]) + self.gamma * (self.sigma * self.fads_proportion * np.minimum(state[:, FADS_INDEX], state[:, ASSET_PRICE_INDEX]))
+        return self.current_state
+
+    def get_arrivals(self) -> np.ndarray:
+        unif = self.rng.uniform(size=(self.num_trajectories, 2))
+        return unif < self.current_state * self.step_size
+
+    def _get_max_arrival_rate(self):
+        return 100 # self.baseline_arrival_rate * 10 TODO: improve this 
